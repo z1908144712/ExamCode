7b5755be23b6020fad5b3178c84bbc238efb2a9f
Unknown_Error <z1908144712@163.com>
2020-09-19 17:20:21 +0800

    update

--- 58同城/T2/main.cpp
+++ 58同城/T2/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <chrono>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-	string t = "2020-06-20 10:10:10.120";
-	time_t tm = 
+	string t = "2020-06-20 10:10:10";
+	string format = "%Y-%m-%d %H:%M:%s";
+	struct tm tma;
+	strftime(const_cast<char*>(t.c_str()), t.size(),format.c_str(), &tma);
+	try {
+		
+	} catch (...) {
+		
+	} 
 	return 0;
 }
